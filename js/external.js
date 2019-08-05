8a49d7165d31489f936a5d32caa191c290424a75
viviancan <vcnales@gmail.com>
2019-08-05 15:18:37 -0500

    Random files from curriculum

---
+++ js/external.js
@@ -0,0 +1,47 @@
+"use strict";
+
+console.log("Hello from external JavaScript.")
+
+// var userColor = prompt("What is your favorite color?")
+// alert("Great, " + userColor + " is my favorite color too!");
+
+/*
+You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
+ */
+
+// var littleMermaidDays = prompt("How long did you rent the little mermaid?")
+// var brotherBearDays = prompt("How long did you rent Brother Bear?")
+// var herculesDays = prompt("How long did you rent Hercules?")
+//
+// var littleMermaidCost = littleMermaidDays * 3;
+// var brotherBearCost = brotherBearDays * 3;
+// var herculesCost = herculesDays * 3;
+//
+// var totalCost = littleMermaidCost + brotherBearCost + herculesCost;
+// alert("Your total is: " + totalCost)
+
+/*
+Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
+  >>>>> Answer: $7420
+*/
+
+// var googleHours = prompt("How many hours did you work at Google?")
+// var amazonHours = prompt("How many hours did you work at Amazon?")
+// var facebookHours = prompt("How many hours did you work at Facebook?")
+//
+// var googlePay = googleHours * 400;
+// var amazonPay = amazonHours * 380
+// var  facebookPay = facebookHours * 350;
+//
+// var totalTechPay = googlePay + amazonPay + facebookPay;
+// alert(totalTechPay);
+
+
+/*
+A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+*/
+
+/*
+A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
+*/
